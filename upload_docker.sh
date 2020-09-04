4d77f71c43b0d9475e51eb5f3e5a53df6c644786
Tamer Tahamoqa <tamerthamoqa@gmail.com>
2020-09-04 17:07:46 +0300

    Add upload_docker.sh

---
+++ upload_docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+dockerpath=tamerthamoqa/udacity-devops-capstone
+
+echo "Docker ID and Image: $dockerpath"
+docker login --username=tamerthamoqa
+docker tag udacity-devops-capstone tamerthamoqa/udacity-devops-capstone
+
+docker push tamerthamoqa/udacity-devops-capstone
